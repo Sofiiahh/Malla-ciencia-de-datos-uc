7fd27f20052d79d193364ce03d2727c9a08e3f8b
Sofía Solorza Madrid <sofiasolorza922@gmail.com>
2025-11-24 19:42:43 -0300

    Create requisitos.py

---
+++ requisitos.py
@@ -0,0 +1,44 @@
+"""
+1er SEMESTRE
+Ningún ramo tiene prerrequisitos.
+IIC1100 – Introducción a la Programación → Sin prerrequisitos
+MAT1100 – Introducción al Cálculo → Sin prerrequisitos
+MAT1200 – Introducción a Álgebra y Geometría → Sin prerrequisitos
+
+2do SEMESTRE
+IIC2133 – Programación Avanzada → Requiere IIC1100
+MAT1201 – Cálculo 1 → Requiere MAT1100
+MAT1302 – Álgebra Lineal para Ciencia de Datos → Requiere IIC1100, MAT1100, MAT1200
+IIC2433 – Introducción a la Ciencia de Datos → Requiere IIC1100, MAT1200
+
+3er SEMESTRE
+MAT2203 – Cálculo para la Ciencia de Datos → Requiere MAT1201
+MAT2301 – Álgebra Lineal Avanzada y Modelamiento → Requiere MAT1201, MAT1302
+EYP280C – Ética para Ciencia de Datos y Estadística → Requiere IIC2133, IIC2433
+MAT2400 – Matemáticas Discretas → Requiere MAT1302
+
+4to SEMESTRE
+MAT2500 – Modelos Probabilísticos → Requiere MAT2203, MAT2301
+IIC2513 – Estructuras de Datos y Algoritmos → Requiere IIC2133, MAT2400
+IIC2413 – Bases de Datos → Requiere MAT2400
+MAT2600 – Optimización para la Ciencia de Datos → Requiere MAT2203, MAT1302
+
+5to SEMESTRE
+MAT2700 – Inferencia Estadística → Requiere MAT2500
+IIC2613 – Inteligencia Artificial → Requiere MAT2500, IIC2133
+IIC2333 – Procesamiento de Datos Masivos → Requiere IIC2413, IIC2513
+
+6to SEMESTRE
+MAT2800 – Procesos Estocásticos Aplicados → Requiere MAT2700
+MAT2810 – Análisis de Regresión → Requiere MAT2700
+IIC2001 – Visualización de la Información → Requiere IIC1100
+IIC2463 – Minería de Datos → Requiere IIC1100, MAT2500, MAT1302
+
+7mo SEMESTRE
+MAT2900 – Simulación → Requiere MAT2800
+MAT2950 – Métodos Bayesianos → Requiere MAT2700
+IIC3253 – Teoría de Aprendizaje Automático → Requiere MAT2600, MAT2500
+
+8vo SEMESTRE
+IIC3900 – Proyecto de Graduación → Requiere MAT2950, IIC3253, MAT2600, EYP280C
+"""
