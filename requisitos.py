549c9c2d8c31aa6e4652f5a2c4ef1478ced38129
Sofía Solorza Madrid <sofiasolorza922@gmail.com>
2025-12-16 17:50:11 -0300

    mejoras  requisitos.py

--- requisitos.py
+++ requisitos.py
@@ -1,5 +1,5 @@
 """
-RAMOS ANUALES:
+RAMOS ANUALES:---------------------------------------------------------------
 
 1er semestre:
 IIC2440 – Procesamiento de Datos Masivos
@@ -11,7 +11,7 @@ FIS154D – Física (probablemente Física General I o similar)
 IMT2200 – Introducción a la Ciencia de Datos
 INT2250 – Optimización para la Ciencia de Datos
 
-
+-----------------------------------------------------------------------------
 
 MALLA Y REQUSITOS
 
@@ -19,47 +19,55 @@ MALLA Y REQUSITOS
 IC1103 – Introducción a la Programación
 MAT1107 – Introducción al Cálculo 
 MAT1207 – Introducción al Álgebra y Geometría 
-MAT0007 – Taller de Matemáticas para Estadística 
+MAT0007 – Taller de Matemáticas para Estadística
+FIL2001 - Filosofia, ¿Para que?
 
 2do SEMESTRE
-IIC2233 – Programación Avanzada → Requiere IC1103
-MAT1610 – Cálculo I → Requiere MAT1107
-IMT2210 – Álgebra Lineal para Ciencia de Datos → Requiere IC1103, MAT1107, MAT1207
-IMT2200 – Introducción a Ciencia de Datos → Requiere IC1103, MAT1207
+IIC2233 – Programación Avanzada → Requiere IC1103 (Introduccion a la programacion)
+MAT1610 – Cálculo I → Requiere MAT1107 (Introduccion al calculo)
+IMT2210 – Álgebra Lineal para Ciencia de Datos → Requiere IC1103, MAT1107, MAT1207 (Introduccion a la programacion; Introduccion al calculo; Introduccion al algebra y geometria)
+IMT2200 – Introducción a Ciencia de Datos → Requiere IC1103, MAT1207 (Introduccion a la programacion; Introduccion al algebra y geometria)
+OFG - Teologico → Sin prerrequisitos
 
 3er SEMESTRE
-IMT2220 – Cálculo para Ciencia de Datos → Requiere MAT1610
-IMT2230 – Álgebra Lineal Avanzada y Modelamiento → Requiere MAT1610, IMT2210
-ETI195 – Ética para Ciencia de Datos y Estadística → Requiere IIC2233, IMT2200
-IIC1253 – Matemáticas Discretas → Requiere IMT2210
+IMT2220 – Cálculo para Ciencia de Datos → Requiere MAT1610 (Calculo 1; #tambien se puede dar en simultaneo a algebra lineal para ciencia de datos)
+IMT2230 – Álgebra Lineal Avanzada y Modelamiento → Requiere MAT1610, IMT2210 (Calculo 1; algebra lineal para ciencia de datos)
+ETI195 – Ética para Ciencia de Datos y Estadística → Requiere IIC2233, IMT2200 (Programacion avanzada; Introduccion a la ciencia de datos)
+IIC1253 – Matemáticas Discretas → Requiere IMT2210 (Algebra lineal para ciencia de datos)
+OFG - Artes  → Sin prerrequisitos
 
 4to SEMESTRE
-EYP1025 – Modelos Probabilísticos → Requiere IMT2220, IMT2230
-IC2133 – Estructuras de Datos y Algoritmos → Requiere IIC2233, IIC1253
-IC2413 – Bases de Datos → Requiere IIC2233, IIC1253
-IMT2250 – Optimización para Ciencia de Datos → Requiere IMT2220, IMT2210
+EYP1025 – Modelos Probabilísticos → Requiere IMT2220, IMT2230 (Calculo para la ciencia de datos; Algebra lineal avanzada y modelamiento)
+IC2133 – Estructuras de Datos y Algoritmos → Requiere IIC2233, IIC1253 (Programacion avanzada; matematicas discretas)
+IC2413 – Bases de Datos → Requiere IIC2233, IIC1253 (Programacion avanzada)
+IMT2250 – Optimización para Ciencia de Datos → Requiere IMT2220, IMT2210 (Calculo para la ciencia de datos; Algebra lineal para la ciencia de datos)
+OFG - Ciencias sociales  → Sin prerrequisitos
 
 5to SEMESTRE
-EYP2114 – Inferencia Estadística → Requiere EYP1025
-IIC2613 – Inteligencia Artificial → Requiere EYP1025, IIC2233
-LIC2440 – Procesamiento de Datos Masivos → Requiere IC2413, IC2133
+EYP2114 – Inferencia Estadística → Requiere EYP1025 (Modelos probabilisticos)
+IIC2613 – Inteligencia Artificial → Requiere EYP1025, IIC2233 (Modelos probabilisticos; Programacion avanzada)
+LIC2440 – Procesamiento de Datos Masivos → Requiere IC2413, IC2133 (Base de datos; Estructura de datos y algoritmos)
 OPR o Minor → Sin prerrequisitos
+OFG - Ecologia integral y sustentabilidad  → Sin prerrequisitos
 
 6to SEMESTRE
-EYP2101 – Procesos Estocásticos Aplicados → Requiere EYP2114
-EYP2301 – Análisis de Regresión → Requiere EYP2114
-C2026 – Visualización de la Información → Requiere IC1103
-IIC2433 – Minería de Datos → Requiere IC1103, EYP1025, IMT2210
+EYP2101 – Procesos Estocásticos Aplicados → Requiere EYP2114 (Inferencia estadistica)
+EYP2301 – Análisis de Regresión → Requiere EYP2114 (Inferencia estadistica)
+C2026 – Visualización de la Información → Requiere IC1103 (Introduccion a la programacion)
+IIC2433 – Minería de Datos → Requiere IC1103, EYP1025, IMT2210 (Introduccion a la programacion; Modelos probabilisticos, Algebra lineal para la ciencia de datos)
+OFG - Humanidades  → Sin prerrequisitos
 
 7mo SEMESTRE
-EYP2111 – Simulación → Requiere EYP2101
-EYP2801 – Métodos Bayesianos → Requiere EYP2114
-IMT2260 – Teoría de Aprendizaje Automático → Requiere IMT2250, EYP1025
+EYP2111 – Simulación → Requiere EYP2101 (Procesos estocaticos aplicados)
+EYP2801 – Métodos Bayesianos → Requiere EYP2114 (Inferencia estadistica)
+IMT2260 – Teoría de Aprendizaje Automático → Requiere IMT2250, EYP1025 (Inferencia estadistica; Optimizacion para la ciencia de datos)
 OPR o Minor → Sin prerrequisitos
+OFG - Salud y Bienestar  → Sin prerrequisitos
 
 8vo SEMESTRE
-IMT2270 – Proyecto de Graduación → Requiere EYP2801, IMT2260, IMT2250, ETI195
+IMT2270 – Proyecto de Graduación → Requiere EYP2801, IMT2260, IMT2250, ETI195 (Metodos bayesianos, Teoria de aprendizaje automatico; Optimizacion para la ciencia de datos; Etica para la ciencia de datos y estadistica)
 OPR o Minor → Sin prerrequisitos
 OPR o Minor → Sin prerrequisitos
 OPR o Minor → Sin prerrequisitos
+OGF - Libre  → Sin prerrequisitos
 """
