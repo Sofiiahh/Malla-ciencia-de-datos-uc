35efb4bfbdc980609e48ddcfd51d3e0e9766b8c1
Sofía Solorza Madrid <sofiasolorza922@gmail.com>
2025-12-17 00:11:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,4 @@
 window.onload = function() {
   crearPlanner();
   mostrarObservaciones();
-
-  const ramos = document.querySelectorAll(".ramo");
-  ramos.forEach(ramo => {
-    ramo.addEventListener("dragstart", e => {
-      e.dataTransfer.setData("text/plain", e.target.id);
-      e.target.classList.add("dragging");
-    });
-    ramo.addEventListener("dragend", e => {
-      e.target.classList.remove("dragging");
-    });
-  });
-
-  const semestresDivs = document.querySelectorAll(".semestre");
-  semestresDivs.forEach(sem => {
-    sem.addEventListener("dragover", e => {
-      e.preventDefault();
-    });
-    sem.addEventListener("drop", e => {
-      e.preventDefault();
-      const id = e.dataTransfer.getData("text/plain");
-      const ramo = document.getElementById(id);
-      sem.appendChild(ramo);
-    });
-  });
 };
