03b399d4422f35f6e96db1399b8d9855d849232c
Sofía Solorza Madrid <sofiasolorza922@gmail.com>
2025-12-17 00:19:15 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,59 +1,59 @@
 const semestres = [
   { nombre: "1er Semestre", ramos: [
-      { codigo: "IC1103", nombre: "Introducción a la Programación", prereq: [], nota: "" },
-      { codigo: "MAT1107", nombre: "Introducción al Cálculo", prereq: [], nota: "" },
-      { codigo: "MAT1207", nombre: "Introducción al Álgebra y Geometría", prereq: [], nota: "" },
-      { codigo: "MAT0007", nombre: "Taller de Matemáticas para Estadística", prereq: [], nota: "" },
-      { codigo: "FIL2001", nombre: "Filosofía, ¿Para qué?", prereq: [], nota: "" }
+      { codigo: "IC1103", nombre: "Introducción a la Programación", prereq: [] },
+      { codigo: "MAT1107", nombre: "Introducción al Cálculo", prereq: [] },
+      { codigo: "MAT1207", nombre: "Introducción al Álgebra y Geometría", prereq: [] },
+      { codigo: "MAT0007", nombre: "Taller de Matemáticas para Estadística", prereq: [] },
+      { codigo: "FIL2001", nombre: "Filosofía, ¿Para qué?", prereq: [] }
   ]},
   { nombre: "2do Semestre", ramos: [
-      { codigo: "IIC2233", nombre: "Programación Avanzada", prereq: ["IC1103"], nota: "" },
-      { codigo: "MAT1610", nombre: "Cálculo I", prereq: ["MAT1107"], nota: "" },
-      { codigo: "IMT2210", nombre: "Álgebra Lineal para Ciencia de Datos", prereq: ["IC1103","MAT1107","MAT1207"], nota: "" },
-      { codigo: "IMT2200", nombre: "Introducción a Ciencia de Datos", prereq: ["IC1103","MAT1207"], nota: "" },
-      { codigo: "OFG - Teológico", nombre: "OFG Teológico", prereq: [], nota: "" }
+      { codigo: "IIC2233", nombre: "Programación Avanzada", prereq: ["IC1103"] },
+      { codigo: "MAT1610", nombre: "Cálculo I", prereq: ["MAT1107"] },
+      { codigo: "IMT2210", nombre: "Álgebra Lineal para Ciencia de Datos", prereq: ["IC1103","MAT1107","MAT1207"] },
+      { codigo: "IMT2200", nombre: "Introducción a Ciencia de Datos", prereq: ["IC1103","MAT1207"] },
+      { codigo: "OFG - Teológico", nombre: "OFG Teológico", prereq: [] }
   ]},
   { nombre: "3er Semestre", ramos: [
-      { codigo: "IMT2220", nombre: "Cálculo para Ciencia de Datos", prereq: ["MAT1610"], nota: "" },
-      { codigo: "IMT2230", nombre: "Álgebra Lineal Avanzada y Modelamiento", prereq: ["MAT1610","IMT2210"], nota: "" },
-      { codigo: "ETI195", nombre: "Ética para Ciencia de Datos y Estadística", prereq: ["IIC2233","IMT2200"], nota: "" },
-      { codigo: "IIC1253", nombre: "Matemáticas Discretas", prereq: ["IMT2210"], nota: "" },
-      { codigo: "OFG - Artes", nombre: "OFG Artes", prereq: [], nota: "" }
+      { codigo: "IMT2220", nombre: "Cálculo para Ciencia de Datos", prereq: ["MAT1610"] },
+      { codigo: "IMT2230", nombre: "Álgebra Lineal Avanzada y Modelamiento", prereq: ["MAT1610","IMT2210"] },
+      { codigo: "ETI195", nombre: "Ética para Ciencia de Datos y Estadística", prereq: ["IIC2233","IMT2200"] },
+      { codigo: "IIC1253", nombre: "Matemáticas Discretas", prereq: ["IMT2210"] },
+      { codigo: "OFG - Artes", nombre: "OFG Artes", prereq: [] }
   ]},
   { nombre: "4to Semestre", ramos: [
-      { codigo: "EYP1025", nombre: "Modelos Probabilísticos", prereq: ["IMT2220","IMT2230"], nota: "" },
-      { codigo: "IC2133", nombre: "Estructuras de Datos y Algoritmos", prereq: ["IIC2233","IIC1253"], nota: "" },
-      { codigo: "IC2413", nombre: "Bases de Datos", prereq: ["IIC2233","IIC1253"], nota: "Puede pedir Matemáticas Discretas en el futuro" },
-      { codigo: "IMT2250", nombre: "Optimización para Ciencia de Datos", prereq: ["IMT2220","IMT2210"], nota: "" },
-      { codigo: "OFG - Ciencias Sociales", nombre: "OFG Ciencias Sociales", prereq: [], nota: "" }
+      { codigo: "EYP1025", nombre: "Modelos Probabilísticos", prereq: ["IMT2220","IMT2230"] },
+      { codigo: "IC2133", nombre: "Estructuras de Datos y Algoritmos", prereq: ["IIC2233","IIC1253"] },
+      { codigo: "IC2413", nombre: "Bases de Datos", prereq: ["IIC2233","IIC1253"] },
+      { codigo: "IMT2250", nombre: "Optimización para Ciencia de Datos", prereq: ["IMT2220","IMT2210"] },
+      { codigo: "OFG - Ciencias Sociales", nombre: "OFG Ciencias Sociales", prereq: [] }
   ]},
   { nombre: "5to Semestre", ramos: [
-      { codigo: "EYP2114", nombre: "Inferencia Estadística", prereq: ["EYP1025"], nota: "" },
-      { codigo: "IIC2613", nombre: "Inteligencia Artificial", prereq: ["EYP1025","IIC2233"], nota: "" },
-      { codigo: "LIC2440", nombre: "Procesamiento de Datos Masivos", prereq: ["IC2413","IC2133"], nota: "" },
-      { codigo: "OPR o Minor", nombre: "OPR/Minor", prereq: [], nota: "" },
-      { codigo: "OFG - Ecología Integral y Sustentabilidad", nombre: "OFG Ecología", prereq: [], nota: "" }
+      { codigo: "EYP2114", nombre: "Inferencia Estadística", prereq: ["EYP1025"] },
+      { codigo: "IIC2613", nombre: "Inteligencia Artificial", prereq: ["EYP1025","IIC2233"] },
+      { codigo: "LIC2440", nombre: "Procesamiento de Datos Masivos", prereq: ["IC2413","IC2133"] },
+      { codigo: "OPR o Minor", nombre: "OPR/Minor", prereq: [] },
+      { codigo: "OFG - Ecología Integral y Sustentabilidad", nombre: "OFG Ecología", prereq: [] }
   ]},
   { nombre: "6to Semestre", ramos: [
-      { codigo: "EYP2101", nombre: "Procesos Estocásticos Aplicados", prereq: ["EYP2114"], nota: "" },
-      { codigo: "EYP2301", nombre: "Análisis de Regresión", prereq: ["EYP2114"], nota: "" },
-      { codigo: "C2026", nombre: "Visualización de la Información", prereq: ["IC1103"], nota: "" },
-      { codigo: "IIC2433", nombre: "Minería de Datos", prereq: ["IC1103","EYP1025","IMT2210"], nota: "" },
-      { codigo: "OFG - Humanidades", nombre: "OFG Humanidades", prereq: [], nota: "" }
+      { codigo: "EYP2101", nombre: "Procesos Estocásticos Aplicados", prereq: ["EYP2114"] },
+      { codigo: "EYP2301", nombre: "Análisis de Regresión", prereq: ["EYP2114"] },
+      { codigo: "C2026", nombre: "Visualización de la Información", prereq: ["IC1103"] },
+      { codigo: "IIC2433", nombre: "Minería de Datos", prereq: ["IC1103","EYP1025","IMT2210"] },
+      { codigo: "OFG - Humanidades", nombre: "OFG Humanidades", prereq: [] }
   ]},
   { nombre: "7mo Semestre", ramos: [
-      { codigo: "EYP2111", nombre: "Simulación", prereq: ["EYP2101"], nota: "" },
-      { codigo: "EYP2801", nombre: "Métodos Bayesianos", prereq: ["EYP2114"], nota: "" },
-      { codigo: "IMT2260", nombre: "Teoría de Aprendizaje Automático", prereq: ["IMT2250","EYP1025"], nota: "" },
-      { codigo: "OPR o Minor", nombre: "OPR/Minor", prereq: [], nota: "" },
-      { codigo: "OFG - Salud y Bienestar", nombre: "OFG Salud y Bienestar", prereq: [], nota: "" }
+      { codigo: "EYP2111", nombre: "Simulación", prereq: ["EYP2101"] },
+      { codigo: "EYP2801", nombre: "Métodos Bayesianos", prereq: ["EYP2114"] },
+      { codigo: "IMT2260", nombre: "Teoría de Aprendizaje Automático", prereq: ["IMT2250","EYP1025"] },
+      { codigo: "OPR o Minor", nombre: "OPR/Minor", prereq: [] },
+      { codigo: "OFG - Salud y Bienestar", nombre: "OFG Salud y Bienestar", prereq: [] }
   ]},
   { nombre: "8vo Semestre", ramos: [
-      { codigo: "IMT2270", nombre: "Proyecto de Graduación", prereq: ["EYP2801","IMT2260","IMT2250","ETI195"], nota: "" },
-      { codigo: "OPR o Minor", nombre: "OPR/Minor 1", prereq: [], nota: "" },
-      { codigo: "OPR o Minor", nombre: "OPR/Minor 2", prereq: [], nota: "" },
-      { codigo: "OPR o Minor", nombre: "OPR/Minor 3", prereq: [], nota: "" },
-      { codigo: "OFG - Libre", nombre: "OFG Libre", prereq: [], nota: "" }
+      { codigo: "IMT2270", nombre: "Proyecto de Graduación", prereq: ["EYP2801","IMT2260","IMT2250","ETI195"] },
+      { codigo: "OPR o Minor", nombre: "OPR/Minor 1", prereq: [] },
+      { codigo: "OPR o Minor", nombre: "OPR/Minor 2", prereq: [] },
+      { codigo: "OPR o Minor", nombre: "OPR/Minor 3", prereq: [] },
+      { codigo: "OFG - Libre", nombre: "OFG Libre", prereq: [] }
   ]}
 ];
 
